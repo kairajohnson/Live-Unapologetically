a2e9b3b0c3c94e6ff69ddeedb740e891bb601c9c
KAIRA N. JOHNSON <KJOHNSON1913@GMAIL.COM>
2016-03-21 12:38:44 -0400

    debugging

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
-Post.destroy_all
-
-post = Post.create!(featured_image: "http://liveunapologetically.com/wp-admin/upload.php?item=917", title: "Test Title", content: "Some content")
-
-post1 = Post.create!(featured_image: "http://liveunapologetically.com/wp-admin/upload.php?item=917", title: "Another Title", content: "Another content")
-
-post2 = Post.create!(featured_image: "http://liveunapologetically.com/wp-admin/upload.php?item=917", title: "Some other Title", content: "Some other body")
